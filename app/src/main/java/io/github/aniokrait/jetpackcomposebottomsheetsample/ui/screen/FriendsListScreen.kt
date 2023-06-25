98eb2bfb758ec200ae439a5713554f60177e5623
Aniokrait <itititcreclickhub@gmail.com>
2023-06-25 17:14:15 +0900

    ボトムシート実装

--- app/src/main/java/io/github/aniokrait/jetpackcomposebottomsheetsample/ui/screen/FriendsListScreen.kt
+++ app/src/main/java/io/github/aniokrait/jetpackcomposebottomsheetsample/ui/screen/FriendsListScreen.kt
@@ -1,8 +1,111 @@
 package io.github.aniokrait.jetpackcomposebottomsheetsample.ui.screen
 
+import androidx.annotation.DrawableRes
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.Button
+import androidx.compose.material.ButtonDefaults
+import androidx.compose.material.ExperimentalMaterialApi
+import androidx.compose.material.ModalBottomSheetLayout
+import androidx.compose.material.ModalBottomSheetValue
+import androidx.compose.material.rememberModalBottomSheetState
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import androidx.compose.ui.zIndex
+import kotlinx.coroutines.launch
 
+@OptIn(ExperimentalMaterial3Api::class, ExperimentalMaterialApi::class)
 @Composable
 fun FriendsListScreen() {
+    var skipHalfExpanded by remember { mutableStateOf(false) }
+    val state = rememberModalBottomSheetState(
+        initialValue = ModalBottomSheetValue.Hidden,
+        skipHalfExpanded = skipHalfExpanded
+    )
+    val scope = rememberCoroutineScope()
+    ModalBottomSheetLayout(
+        modifier = Modifier.zIndex(100F),
+        sheetElevation = 60.dp,
+        sheetState = state,
+        sheetContent = {
+//            //ボトムシート内に表示するコンテンツ
+//            Column(
+//                modifier = Modifier.padding(top = 16.dp, bottom = 16.dp, start = 8.dp),
+//                verticalArrangement = Arrangement.SpaceBetween
+//            ) {
+//                BottomSheetIconTextRow(icon = R.drawable.baseline_share_24, text = "Share")
+//                BottomSheetIconTextRow(icon = R.drawable.baseline_link_24, text = "Get link")
+//                BottomSheetIconTextRow(icon = R.drawable.baseline_edit_24, text = "Edit name")
+//                BottomSheetIconTextRow(icon = R.drawable.baseline_delete_24, text = "Delete collection")
+//            }
+            var sheetCategory = BottomSheetCategory.SHEET_A
+            Column {
+                when(sheetCategory) {
+                    BottomSheetCategory.SHEET_A -> {
+                        //ボタンAに対応したボトムシートを表示
+                    }
+                    BottomSheetCategory.SHEET_B -> {
+                        //ボタンBに対応したボトムシートを表示
+                    }
+                }
 
+            }
+        }
+    ) {
+        //ボトムシートを開く画面はModalBottomSheetLayoutでラップする必要があった。
+        Column(
+            modifier = Modifier
+                .fillMaxSize()
+                .padding(16.dp),
+            horizontalAlignment = Alignment.CenterHorizontally,
+            verticalArrangement = Arrangement.Center,
+        ) {
+            Text(text = "主画面のコンテンツ")
+            Button(
+                modifier = Modifier.padding(bottom = 16.dp),
+                colors = ButtonDefaults.textButtonColors(
+                    backgroundColor = Color.Gray,
+                    contentColor = Color.White
+                ),
+                onClick = { scope.launch { state.show() } }) {
+                Text("ボトムシートを開く")
+            }
+        }
+    }
+}
+
+@Composable
+fun BottomSheetIconTextRow(@DrawableRes icon: Int, text: String) {
+    Row(
+        verticalAlignment = Alignment.CenterVertically
+    ) {
+        Icon(painterResource(icon), text, tint = Color.Gray)
+        Text(
+            modifier = Modifier.padding(start = 20.dp),
+            text = text,
+            fontSize = 24.sp,
+        )
+    }
+
+}
+
+enum class BottomSheetCategory{
+    SHEET_A,
+    SHEET_B
 }
--- app/src/main/java/io/github/aniokrait/jetpackcomposebottomsheetsample/ui/screen/ProfileScreen.kt
+++ app/src/main/java/io/github/aniokrait/jetpackcomposebottomsheetsample/ui/screen/ProfileScreen.kt
@@ -2,20 +2,77 @@ package io.github.aniokrait.jetpackcomposebottomsheetsample.ui.screen
 
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Button
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.ModalBottomSheet
+import androidx.compose.material3.Switch
 import androidx.compose.material3.Text
+import androidx.compose.material3.rememberModalBottomSheetState
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+import kotlinx.coroutines.launch
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun ProfileScreen() {
+    var openBottomSheet by rememberSaveable { mutableStateOf(false) }
+    var skipPartiallyExpanded by remember { mutableStateOf(false) }
+    val scope = rememberCoroutineScope()
+    val bottomSheetState = rememberModalBottomSheetState(
+        skipPartiallyExpanded = skipPartiallyExpanded
+    )
+
     Column(
         modifier = Modifier.fillMaxSize(),
         verticalArrangement = Arrangement.Center,
         horizontalAlignment = Alignment.CenterHorizontally
     ) {
-        Text(text = "プロフィール画面")
+        Text(text = "主画面のコンテンツ")
+        Button(onClick = {
+            scope.launch {
+                openBottomSheet = true
+            }
+        }) {
+            Text(text = "ボトムシートを開く")
+        }
     }
+    //ここからボトムシートのコンテンツ
+    if (openBottomSheet) {
+        ModalBottomSheet(
+            modifier = Modifier.padding(top = 16.dp),
+            onDismissRequest = {openBottomSheet = false },
+            sheetState = bottomSheetState,
+        ) {
+            Column(modifier = Modifier.fillMaxSize()) {
+                Row(
+                    verticalAlignment = Alignment.CenterVertically,
+                    horizontalArrangement = Arrangement.SpaceBetween
+                ) {
+                    Text(text = "なんかの設定項目")
+                    Switch(checked = false, onCheckedChange = { })
+                }
+                Spacer(modifier = Modifier)
+                Row(
+                    verticalAlignment = Alignment.CenterVertically
+                ) {
+                    Text(text = "なんかの設定項目2")
+                    Switch(checked = true, onCheckedChange = { })
+                }
+            }
+        }
+    }
+
 }
 
